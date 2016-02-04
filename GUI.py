4d97f08141f336df2e9e3739e2db1886c5346420
jordan139 <jordan139@hotmail.co.uk>
2016-02-04 20:22:18 +0000

    Add more features here

--- GUI.py
+++ GUI.py
@@ -1,1 +1,26 @@
-''' add GUI code here, create a basic GUI with a live loop '''
+from Tkinter import *
+
+root = Tk()
+
+topFrame = Frame(root)
+topFrame.pack()
+bottomFrame = Frame(root)
+bottomFrame.pack(side = BOTTOM)
+
+def button(buttonText, fgcolour,bgcolour):
+    button1 = Button(topFrame, text= buttonText, fg = fgcolour, bg = bgcolour)
+    button1.pack(side = LEFT)
+    
+
+def label(textOne):
+    label1 = Label(topFrame, text = textOne)
+    label1.pack()
+
+    
+
+
+button('Hello', 'white','black')
+button('World' ,'blue', 'orange')
+button('!', 'black', 'cyan')
+label('dhujdhjsdj') 
+root = mainloop()
