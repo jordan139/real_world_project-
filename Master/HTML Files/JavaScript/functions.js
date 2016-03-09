07fd31fc17ac357acfb33364c353dce84ad728a9
bradasxd <bradleyrpxd@gmail.com>
2016-03-09 15:36:20 +0000

    added jquery functionailty with test element (blue square animating)

--- Master/HTML Files/JavaScript/functions.js
+++ Master/HTML Files/JavaScript/functions.js
@@ -1,4 +1,10 @@
-function printFunction()
-	{
-		window.print()
-}
+// Any time an element with the class of "tagline" is clicked
+$(document).ready(function(){
+    $("div").mouseenter(function(){
+        $("div").fadeTo("fast",0.5);
+    });
+    $("div").mouseleave(function(){
+        $("div").fadeTo("fast",1);
+    });    
+});
+
