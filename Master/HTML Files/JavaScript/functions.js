c95a94e335d0efd6ecac727b8dbc03ec873d9d23
bradasxd <bradleyrpxd@gmail.com>
2016-03-09 20:26:35 +0000

    fixed css issues with header bar on cirlce gui as well as continuing testing Jquery functionailty

--- Master/HTML Files/JavaScript/functions.js
+++ Master/HTML Files/JavaScript/functions.js
@@ -1,5 +1,5 @@
 // Any time an element with the class of "tagline" is clicked
-$(document).ready(function(){
+$(function(){
     $("div").mouseenter(function(){
         $("div").fadeTo("fast",0.5);
     });
@@ -7,4 +7,26 @@ $(document).ready(function(){
         $("div").fadeTo("fast",1);
     });    
 });
+/* sample statement for modifying css in Jquery
+$(document).ready(function(){
+   $(".HeaderBox").css("background-color", "#"); 
+});
 
+*/
+
+
+$(document).ready(function(){
+    $(".Header").mouseenter(function(){
+       $(".Header").animate({
+           fontSize:"+=20pt"
+       },2000, "linear"); 
+    });
+});
+
+/* hiding function
+$(".Header").animate({
+    fontSize: "+=20pt"
+    }
+    , 2000);
+    
+*/
